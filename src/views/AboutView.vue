030c928c5ed53a678ea207a210c112f594a5e5a5
mquanaazi <olwethumalumla31@gmail.com>
2024-07-01 14:30:47 +0200

    added my navbar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+
+  
+  <div id='content'> 
+    <about-comp/>
+ 
   </div>
 </template>
+<script>
+import AboutComp from "../components/AboutComp.vue";
+export default {
+ component: {
+  AboutComp,
+  }
+}
+</script>
+<style >
+#content{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+</style>
